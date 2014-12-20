4b3bcc25e3ec1dd46310d0e646ea6d61d00b23e5
Hector Buelta <Hector@Hectors-MacBook-Pro.local>
2014-12-20 20:23:25 +0100

    Removed other boards support. Localised for Arduino Boards

--- Arduino_Test_UNO/Arduino_Test_UNO.ino
+++ Arduino_Test_UNO/Arduino_Test_UNO.ino
@@ -17,29 +17,8 @@
 //
 
 // Core library for code-sense - IDE-based
-#if defined(WIRING) // Wiring specific
-#include "Wiring.h"
-#elif defined(MAPLE_IDE) // Maple specific
-#include "WProgram.h"
-#elif defined(MPIDE) // chipKIT specific
-#include "WProgram.h"
-#elif defined(DIGISPARK) // Digispark specific
-#include "Arduino.h"
-#elif defined(ENERGIA) // LaunchPad specific
-#include "Energia.h"
-#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
-#include "LRF.h"
-#elif defined(MICRODUINO) // Microduino specific
-#include "Arduino.h"
-#elif defined(TEENSYDUINO) // Teensy specific
-#include "Arduino.h"
-#elif defined(REDBEARLAB) // RedBearLab specific
-#include "Arduino.h"
-#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
-#include "Arduino.h"
-#else // error
-#error Platform not defined
-#endif // end IDE
+
+#include <Arduino.h>
 
 // Include application, user and local libraries
 
@@ -59,20 +38,9 @@ uint8_t myLED;
 // Add setup code
 
 void setup() {
-  // myLED pin number
-#if defined(ENERGIA) // All LaunchPads supported by Energia
-    myLED = RED_LED;
-#elif defined(DIGISPARK) // Digispark specific
-    myLED = 1; // assuming model A
-#elif defined(MAPLE_IDE) // Maple specific
-    myLED = BOARD_LED_PIN;
-#elif defined(WIRING) // Wiring specific
-    myLED = 15;
-#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
-    myLED = 10;
-#else // Arduino, chipKIT, Teensy specific
+  // myLED pin number (13 on most arduino boards)
+
     myLED = 13;
-#endif
 
     pinMode(myLED, OUTPUT);
     
